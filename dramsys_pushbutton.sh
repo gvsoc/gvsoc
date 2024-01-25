b2b3a7d4d464a58b1cebcf63a9141b52578bdec1
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-01-25 18:12:57 +0100

    [Test] add test binary for DMA-DRAM testing

--- dramsys_pushbutton.sh
+++ dramsys_pushbutton.sh
@@ -2,4 +2,5 @@ make dramsys_preparation
 pip3 install -r core/requirements.txt
 pip3 install -r gapy/requirements.txt
 source sourceme.sh
-# make TARGETS=pulp-open-ddr all
+make TARGETS=pulp-open-ddr all
+gvsoc --target=pulp-open-ddr --binary add_dramsyslib_patches/dma_dram_test.bin image flash run --trace=ddr
