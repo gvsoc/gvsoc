63114542e26a68d05d2980758c75058e34aa87a0
Kexin Li <likexi@student.ethz.ch>
2024-04-29 14:08:46 +0200

    [Snitch Cluster] Add runtime library for compilation

---
+++ sw/runtime/common/snitch_cluster_cfg.h
@@ -0,0 +1,6 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#define CFG_CLUSTER_NR_CORES 9
+#define CFG_CLUSTER_BASE_HARTID 0
