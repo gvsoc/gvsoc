79568ae622233d0af6194a3ac59b44827088384b
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2024-07-03 16:48:45 +0200

    env: added sourceme_systemc to avoid systemc by default

--- sourceme_systemc.sh
+++ sourceme_systemc.sh
@@ -11,4 +11,6 @@ export PYTHONPATH=$SDK_HOME/install/python:$PYTHONPATH
 ##############################################################################
 ## 				Envirment Parameters for DRAMSys Integration 				##
 ##############################################################################
+export SYSTEMC_HOME=$SDK_HOME/third_party/systemc_install
+export LD_LIBRARY_PATH=${SYSTEMC_HOME}/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SDK_HOME/third_party/DRAMSys:$LD_LIBRARY_PATH
