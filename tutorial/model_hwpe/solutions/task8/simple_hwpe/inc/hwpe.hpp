arpansur/gvsoc-test

[Tutorial] Added files - Needs testing

tutorial/model_hwpe/solutions/task8/simple_hwpe/inc/hwpe.hpp
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2020-2022  GreenWaves Technologies, ETH Zurich, University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Authors: Arpan Suravi Prasad, ETH Zurich (prasadar@iis.ee.ethz.ch)
+ */
+#ifndef __HWPE_HPP__
+#define __HWPE_HPP__
+#include <vp/vp.hpp>
+#include <vp/itf/io.hpp>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <assert.h>
+#include <string>
+#include <bitset>
+#include "params.hpp"
+#include "datatype.hpp"
+#include "regconfig_manager.hpp"
+class Hwpe : public vp::Component
+{
+public:
+  Hwpe(vp::ComponentConf &config);
+  vp::IoMaster tcdm_port;
+  vp::Trace trace;
+  vp::reg_32 state;
+
+
+  //register configuration instance
+  RegConfigManager<Hwpe> regconfig_manager_instance;
+
+private:
+  vp::IoSlave cfg_port_;
+  vp::WireMaster<bool> irq;
+
+  Regconfig reg_config_;
+
+
+  Iterator input, weight, compute, output;
+
+  //////////////////// DATAPATH RELATED DATA STRUCTURE ////////////////////
+
+  LinearBuffer<InputType, INPUT_LINEAR_BUFFER_SIZE> input_buffer_;
+  LinearBuffer<OutputType, OUTPUT_LINEAR_BUFFER_SIZE> output_buffer_;
+  PEComputeUnit<Hwpe, InputType, WeightType, OutputType, COLUMN_PER_PE, BINCONV_PER_COLUMN> pe_instance_;
+
+  std::array<std::array<InputType, BINCONV_PER_COLUMN>, COLUMN_PER_PE> input_layout_;
+  std::array<WeightType, BINCONV_PER_COLUMN*COLUMN_PER_PE/8> weight_temp_buf_;
+  std::array<std::array<WeightType, BINCONV_PER_COLUMN>,COLUMN_PER_PE> weight_temp_layout_;
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  void clear();
+  int  fsm();
+  void fsm_loop();
+
+  void reset();
+
+  int64_t input_load();
+  void    input_layout();
+
+  int64_t weight_load();
+  void    weight_layout();
+  
+  int64_t weight_offset();
+  
+  int64_t compute();
+  int64_t output_store();
+
+  static vp::IoReqStatus hwpe_slave(vp::Block *__this, vp::IoReq *req);
+
+  vp::ClockEvent *fsm_start_event;
+  vp::ClockEvent *fsm_event;
+  vp::ClockEvent *fsm_end_event;
+
+  static void FsmStartHandler(vp::Block *__this, vp::ClockEvent *event);
+  static void FsmHandler(vp::Block *__this, vp::ClockEvent *event);
+  static void FsmEndHandler(vp::Block *__this, vp::ClockEvent *event);
+};
+#endif /* __HWPE_HPP__ */

tutorial/model_hwpe/solutions/task8/simple_hwpe/src/hwpe_compute.cpp
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2020-2022  GreenWaves Technologies, ETH Zurich, University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Authors: Arpan Suravi Prasad, ETH Zurich (prasadar@iis.ee.ethz.ch)
+ */
+#include "hwpe.hpp"
+
+int64_t Hwpe::compute()
+{
+  std::array<std::array<bool, BINCONV_PER_COLUMN>, COLUMN_PER_PE> enable;
+  std::array<std::array<WeightType, BINCONV_PER_COLUMN>, COLUMN_PER_PE> weight;
+  std::array<OutputType, COLUMN_PER_PE> sum_array;
+  std::array<WeightType, COLUMN_PER_PE> shift;
+
+  OutputType sum = this->output_buffer_.ReadFromIndex(this->compute.iteration);
+
+  for(int i=0; i<COLUMN_PER_PE; i++) {
+      enable[i].fill(true);
+      shift[i] = i;
+  }
+  this->pe_instance_.ComputePartialSum(enable, this->input_layout_, this->weight_temp_layout_, shift, sum_array, sum);
+
+  this->output_buffer_.WriteAtIndex(this->compute.iteration, 1, sum);
+  this->compute.iteration++;
+  return 0;
+}

gvsoc/gvsoc

tutorial/model_hwpe/solutions/task8/simple_hwpe/inc/hwpe.hpp
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2020-2022  GreenWaves Technologies, ETH Zurich, University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Authors: Arpan Suravi Prasad, ETH Zurich (prasadar@iis.ee.ethz.ch)
+ */
+#ifndef __HWPE_HPP__
+#define __HWPE_HPP__
+#include <vp/vp.hpp>
+#include <vp/itf/io.hpp>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <assert.h>
+#include <string>
+#include <bitset>
+#include "params.hpp"
+#include "datatype.hpp"
+#include "scalar_buffer.hpp"
+#include "linear_buffer.hpp"
+#include "binconv.hpp"
+#include "column.hpp"
+#include "pe_compute_unit.hpp"
+#include "regconfig_manager.hpp"
+class Hwpe : public vp::Component
+{
+public:
+  Hwpe(vp::ComponentConf &config);
+  vp::IoMaster tcdm_port;
+  vp::Trace trace;
+  vp::reg_32 state;
+  vp::IoReq io_req;
+
+
+  //register configuration instance
+  RegConfigManager<Hwpe> regconfig_manager_instance;
+
+private:
+  vp::IoSlave cfg_port_;
+  vp::WireMaster<bool> irq;
+
+  Regconfig reg_config_;
+
+
+  Iterator input, weight, compute, output;
+
+  //////////////////// DATAPATH RELATED DATA STRUCTURE ////////////////////
+
+  LinearBuffer<InputType, INPUT_LINEAR_BUFFER_SIZE> input_buffer_;
+  LinearBuffer<OutputType, OUTPUT_LINEAR_BUFFER_SIZE> output_buffer_;
+  PEComputeUnit<Hwpe, InputType, WeightType, OutputType, COLUMN_PER_PE, BINCONV_PER_COLUMN> pe_instance_;
+
+  std::array<std::array<InputType, BINCONV_PER_COLUMN>, COLUMN_PER_PE> input_layout_;
+  std::array<WeightType, BINCONV_PER_COLUMN*COLUMN_PER_PE/8> weight_temp_buf_;
+  std::array<std::array<WeightType, BINCONV_PER_COLUMN>,COLUMN_PER_PE> weight_temp_layout_;
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  void clear();
+  int  fsm();
+  void fsm_loop();
+
+  void reset();
+
+  int64_t input_load();
+  void    input_layout();
+
+  int64_t weight_load();
+  void    weight_layout();
+  
+  int64_t weight_offset();
+  
+  int64_t compute_output();
+  int64_t output_store();
+
+  std::string HwpeStateToString(const HwpeState&);
+  static vp::IoReqStatus hwpe_slave(vp::Block *__this, vp::IoReq *req);
+
+  vp::ClockEvent *fsm_start_event;
+  vp::ClockEvent *fsm_event;
+  vp::ClockEvent *fsm_end_event;
+
+  static void FsmStartHandler(vp::Block *__this, vp::ClockEvent *event);
+  static void FsmHandler(vp::Block *__this, vp::ClockEvent *event);
+  static void FsmEndHandler(vp::Block *__this, vp::ClockEvent *event);
+};
+#endif /* __HWPE_HPP__ */

tutorial/model_hwpe/solutions/task8/simple_hwpe/src/hwpe_compute.cpp
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2020-2022  GreenWaves Technologies, ETH Zurich, University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Authors: Arpan Suravi Prasad, ETH Zurich (prasadar@iis.ee.ethz.ch)
+ */
+#include "hwpe.hpp"
+
+int64_t Hwpe::compute_output()
+{
+  std::array<std::array<bool, BINCONV_PER_COLUMN>, COLUMN_PER_PE> enable;
+  std::array<std::array<WeightType, BINCONV_PER_COLUMN>, COLUMN_PER_PE> weight;
+  std::array<OutputType, COLUMN_PER_PE> sum_array;
+  std::array<WeightType, COLUMN_PER_PE> shift;
+
+  OutputType sum = this->output_buffer_.ReadFromIndex(this->compute.iteration);
+
+  for(int i=0; i<COLUMN_PER_PE; i++) {
+      enable[i].fill(true);
+      shift[i] = i;
+  }
+  Mode mode;
+  this->pe_instance_.ComputePartialSum(mode, enable, this->input_layout_, this->weight_temp_layout_, shift, sum_array, sum);
+
+  this->output_buffer_.WriteAtIndex(this->compute.iteration, 1, sum);
+  this->compute.iteration++;
+  return 0;
+}