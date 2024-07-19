dcdd5368842950163e3eccb6dd70ff8379dd76c6
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-07-19 09:26:24 +0200

    [SoftHier] flex cluster: [SW] add simple GEMM implementation

--- add_dramsyslib_patches/flex_cluster_sdk/test/include/kernels/llm_mlp/llm_mlp_matmul.h
+++ add_dramsyslib_patches/flex_cluster_sdk/test/include/kernels/llm_mlp/llm_mlp_matmul.h
@@ -11,8 +11,8 @@
 void llm_mlp_matmul_routine(uint32_t M, uint32_t N, uint32_t K, uint32_t tile_size_byte)
 {
     if (flex_is_dm_core()){
-        flex_dma_pattern_access_west_hbm(tile_size_byte, 0, tile_size_byte);
-        flex_dma_pattern_access_west_hbm(tile_size_byte*2, 0, tile_size_byte);
+        flex_dma_pattern_access_west_hbm(0, 0, tile_size_byte);
+        flex_dma_pattern_access_west_hbm(0, 0, tile_size_byte);
     }
 
 	if (flex_is_first_core())
@@ -27,8 +27,8 @@ void llm_mlp_matmul_routine(uint32_t M, uint32_t N, uint32_t K, uint32_t tile_si
 void llm_mlp_matmul_preload(uint32_t tile_size_byte)
 {
     if (flex_is_dm_core()){
-        flex_dma_pattern_access_west_hbm(tile_size_byte, 0, tile_size_byte);
-        flex_dma_pattern_access_west_hbm(tile_size_byte*2, 0, tile_size_byte);
+        flex_dma_pattern_access_west_hbm(0, 0, tile_size_byte);
+        flex_dma_pattern_access_west_hbm(0, 0, tile_size_byte);
     }
 }
 
@@ -68,24 +68,17 @@ void llm_mlp_matmul(uint32_t num_iter, uint32_t tile_dimension, uint32_t elem_si
 	//Final compute
 	llm_mlp_matmul_final_compute(tile_dimension,tile_dimension,tile_dimension);
 	snrt_cluster_hw_barrier();
-
-	//Store output matrix
-	llm_mlp_matmul_store(tile_size_byte);
-	snrt_cluster_hw_barrier();
 }
 
-void llm_mlp_test(uint32_t tile_dimension)
+void llm_mlp_test(uint32_t tile_dimension, uint32_t elem_size, uint32_t num_iter)
 {
-    uint32_t num_iter = 4096/tile_dimension;
-    uint32_t num_matmul = (8192/(tile_dimension*ARCH_NUM_CLUSTER_Y)) * (16384/(tile_dimension*ARCH_NUM_CLUSTER_X));
 
     flex_global_barrier_xy();
     flex_timer_start();
 
-    for (int i = 0; i < num_matmul; ++i)
-    {
-        llm_mlp_matmul(num_iter, tile_dimension, 2);
-    }
+
+    llm_mlp_matmul(num_iter, tile_dimension, elem_size);
+
 
     flex_global_barrier_xy();
     flex_timer_end();
--- add_dramsyslib_patches/flex_cluster_sdk/test/src/test.c
+++ add_dramsyslib_patches/flex_cluster_sdk/test/src/test.c
@@ -5,6 +5,7 @@
 #include "flex_dma_pattern.h"
 #include "kernels/common/common_tiling.h"
 #include "kernels/llm_mlp/llm_mlp_matmul.h"
+#include "kernels/llm_mlp/llm_mlp_inter_cluster_matmul.h"
 #include <math.h>
 
 int main()
@@ -18,7 +19,11 @@ int main()
     /**************************************/
 
     // llm_mlp_matmul(10, 64, 2);
-    llm_mlp_test(64);
+    // llm_mlp_test(2048);
+    // llm_mlp_inter_cluster_test_hbm(128,2);
+    llm_mlp_inter_cluster_matmul(1024,2,ARCH_NUM_CLUSTER_X);
+    // llm_mlp_inter_cluster_matmul_optimize_hbm(1024,2,ARCH_NUM_CLUSTER_X);
+    // llm_mlp_test(1024,2,ARCH_NUM_CLUSTER_X);
 
     /**************************************/
     /*  Program Execution Region -- Stop  */
