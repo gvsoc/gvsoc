3fd8165eeadbeeb9da51f1959cf7dcc5ce451851
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-09-28 16:10:41 +0200

    [SoftHier] update example arch

--- add_dramsyslib_patches/flex_cluster/flex_cluster_arch.py
+++ add_dramsyslib_patches/flex_cluster/flex_cluster_arch.py
@@ -56,7 +56,7 @@ class FlexClusterArch:
         self.hbm_start_base          = 0xc0000000
         self.hbm_node_interleave     = 0x00100000
         self.num_hbm_ch_per_node     = 1
-        self.hbm_placement           = [4,0,0,4]
+        self.hbm_placement           = [4,0,0,0]
 
         #NoC
         self.noc_outstanding         = 64
