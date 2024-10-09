1b8f0566ff9bc3d5fbdf0235bcc48428a92db42f
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-10-09 13:56:52 +0200

    [SoftHier] update example SW

--- add_dramsyslib_patches/flex_cluster_sdk/test/include/examples/example_one_cluster_gemm.h
+++ add_dramsyslib_patches/flex_cluster_sdk/test/include/examples/example_one_cluster_gemm.h
@@ -30,26 +30,29 @@
 #define YZ_L1_OFFSET   (W_L1_OFFSET2 + TILE_SIZE_BYTE)
 
 void example_one_cluster_gemm(){
-    flex_global_barrier_xy();                                                                               //Global barrier
+    flex_global_barrier_xy();//Global barrier
 
-    uint32_t CID = flex_get_cluster_id();                                                                   //Get cluster ID
+    uint32_t CID = flex_get_cluster_id();//Get cluster ID
 
-                                                                                                            //Initialize RedMule Paramters
-    if (flex_is_first_core() && (CID == 0))                                                                 //Use the first core in cluster 0 to configure RedMule
+    //Initialize RedMule Paramters
+    if (flex_is_first_core() && (CID == 0))//Use the first core in cluster 0 to configure RedMule
     {
-        flex_redmule_set_M(0, TILE_DIMENSION);                                                              //Configure M-N-K of tile that RedMule will accelerate
+        //Configure M-N-K of tile that RedMule will accelerate
+        flex_redmule_set_M(0, TILE_DIMENSION);
         flex_redmule_set_N(0, TILE_DIMENSION);
         flex_redmule_set_K(0, TILE_DIMENSION);
     }
 
-    flex_global_barrier_xy();                                                                               //Global barrier
+    flex_global_barrier_xy();//Global barrier
 
-    if (CID == 0)                                                                                           //Only let Cluster 0 to work
+    if (CID == 0)//Only let Cluster 0 to work
     {
-        for (int row = 0; row < TILES_PER_DIM; ++row)                                                       //Iterate over every Z tiles
+        //Iterate over every Z tiles
+        for (int row = 0; row < TILES_PER_DIM; ++row)
         {
             for (int col = 0; col < TILES_PER_DIM; ++col)
-            {                                                                                               //Start address of tiles
+            {
+                //Start address of tiles
                 uint32_t X_hbm_addr = X_HBM_OFFSET + row * TILES_PER_DIM * TILE_SIZE_BYTE;
                 uint32_t W_hbm_addr = W_HBM_OFFSET + col * TILE_SIZE_BYTE;
                 uint32_t Y_hbm_addr = Y_HBM_OFFSET + row * TILES_PER_DIM * TILE_SIZE_BYTE + col * TILE_SIZE_BYTE;
@@ -57,54 +60,84 @@ void example_one_cluster_gemm(){
 
 
                 //Preload Y,X,W tiles
-                if (flex_is_dm_core()){                                                                     //Use DM core to trigger DMA transcations
-                    flex_dma_async_1d(local(YZ_L1_OFFSET),hbm_addr(Y_hbm_addr), TILE_SIZE_BYTE);            //Trigger DMA transaction: move Y tile from HBM to L1
-                    flex_dma_async_1d(local(X_L1_OFFSET1),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);            //Trigger DMA transaction: move X tile from HBM to L1
-                    flex_dma_async_1d(local(W_L1_OFFSET1),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);            //Trigger DMA transaction: move W tile from HBM to L1
-                    flex_dma_async_wait_all();                                                              //Wait all DMA transaction done     
+                if (flex_is_dm_core()){//Use DM core to trigger DMA transcations
+                    //Trigger DMA transaction: move Y tile from HBM to L1
+                    flex_dma_async_1d(local(YZ_L1_OFFSET),hbm_addr(Y_hbm_addr), TILE_SIZE_BYTE);
+
+                    //Trigger DMA transaction: move X tile from HBM to L1
+                    flex_dma_async_1d(local(X_L1_OFFSET1),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);
+
+                    //Trigger DMA transaction: move W tile from HBM to L1
+                    flex_dma_async_1d(local(W_L1_OFFSET1),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);
+
+                    //Wait all DMA transaction done
+                    flex_dma_async_wait_all();
                 }
-                flex_intra_cluster_sync();                                                                  //Cluster barrier
+                flex_intra_cluster_sync();//Cluster barrier
 
                 //Compute X and W to YZ in L1 + preload next X and W
                 for (int i = 0; i < (TILES_PER_DIM-1); ++i)
                 {
-                    X_hbm_addr += TILE_SIZE_BYTE;                                                           //Calculate next X and W tile address in HBM
+                    //Calculate next X and W tile address in HBM
+                    X_hbm_addr += TILE_SIZE_BYTE;
                     W_hbm_addr += TILES_PER_DIM * TILE_SIZE_BYTE;
-                    if (i%2 == 0)                                                                           //Ping-Pong operations on Double-Buffering X and W
+
+                    //Ping-Pong operations on Double-Buffering X and W
+                    if (i%2 == 0)
                     {
-                        if (flex_is_dm_core()){                                                             //Use DM core to trigger DMA transcations
-                            flex_dma_async_1d(local(X_L1_OFFSET2),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);    //Trigger DMA transaction: move X tile from HBM to L1
-                            flex_dma_async_1d(local(W_L1_OFFSET2),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);    //Trigger DMA transaction: move W tile from HBM to L1
-                            flex_dma_async_wait_all();                                                      //Wait all DMA transaction done
+                        if (flex_is_dm_core()){//Use DM core to trigger DMA transcations
+                            //Trigger DMA transaction: move X tile from HBM to L1
+                            flex_dma_async_1d(local(X_L1_OFFSET2),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);
+
+                            //Trigger DMA transaction: move W tile from HBM to L1
+                            flex_dma_async_1d(local(W_L1_OFFSET2),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);
+
+                            //Wait all DMA transaction done
+                            flex_dma_async_wait_all();
                         }
 
-                        if (flex_is_first_core())                                                           //Use the first core in cluster 0 to configure and trigger RedMule
+                        if (flex_is_first_core())//Use the first core in cluster 0 to configure and trigger RedMule
                         {
-                            flex_redmule_set_X(0, X_L1_OFFSET1);                                            //Configure tile address in L1
+                            //Configure tile address in L1
+                            flex_redmule_set_X(0, X_L1_OFFSET1);
                             flex_redmule_set_W(0, W_L1_OFFSET1);
                             flex_redmule_set_Y(0, YZ_L1_OFFSET);
                             flex_redmule_set_Z(0, YZ_L1_OFFSET);
-                            flex_redmule_trigger_async(0);                                                  //Run RedMule Acceleration
-                            flex_redmule_trigger_wait(0);                                                   //Wait RedMule Done
+
+                            //Run RedMule Acceleration
+                            flex_redmule_trigger_async(0);
+
+                            //Wait RedMule Done
+                            flex_redmule_trigger_wait(0);
                         }
                     } else {
-                        if (flex_is_dm_core()){                                                             //Use DM core to trigger DMA transcations
-                            flex_dma_async_1d(local(X_L1_OFFSET1),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);    //Trigger DMA transaction: move X tile from HBM to L1
-                            flex_dma_async_1d(local(W_L1_OFFSET1),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);    //Trigger DMA transaction: move W tile from HBM to L1
-                            flex_dma_async_wait_all();                                                      //Wait all DMA transaction done
+                        if (flex_is_dm_core()){//Use DM core to trigger DMA transcations
+                            //Trigger DMA transaction: move X tile from HBM to L1
+                            flex_dma_async_1d(local(X_L1_OFFSET1),hbm_addr(X_hbm_addr), TILE_SIZE_BYTE);
+
+                            //Trigger DMA transaction: move W tile from HBM to L1
+                            flex_dma_async_1d(local(W_L1_OFFSET1),hbm_addr(W_hbm_addr), TILE_SIZE_BYTE);
+
+                            //Wait all DMA transaction done
+                            flex_dma_async_wait_all();
                         }
 
-                        if (flex_is_first_core())                                                           //Use the first core in cluster 0 to configure and trigger RedMule
+                        if (flex_is_first_core())//Use the first core in cluster 0 to configure and trigger RedMule
                         {
-                            flex_redmule_set_X(0, X_L1_OFFSET2);                                            //Configure tile address in L1
+                            //Configure tile address in L1
+                            flex_redmule_set_X(0, X_L1_OFFSET2);
                             flex_redmule_set_W(0, W_L1_OFFSET2);
                             flex_redmule_set_Y(0, YZ_L1_OFFSET);
                             flex_redmule_set_Z(0, YZ_L1_OFFSET);
-                            flex_redmule_trigger_async(0);                                                  //Run RedMule Acceleration
-                            flex_redmule_trigger_wait(0);                                                   //Wait RedMule Done
+
+                            //Run RedMule Acceleration
+                            flex_redmule_trigger_async(0);
+
+                            //Wait RedMule Done
+                            flex_redmule_trigger_wait(0);
                         }
                     }
-                    flex_intra_cluster_sync();                                                              //Cluster barrier
+                    flex_intra_cluster_sync();//Cluster barrier
                 }
 
                 //Last Computation
@@ -117,19 +150,22 @@ void example_one_cluster_gemm(){
                     flex_redmule_trigger_async(0);
                     flex_redmule_trigger_wait(0);
                 }
-                flex_intra_cluster_sync();                                                                  //Cluster barrier
+                flex_intra_cluster_sync();//Cluster barrier
 
                 //Store Z tile
                 if (flex_is_dm_core()){
-                    flex_dma_async_1d(hbm_addr(Z_hbm_addr),local(YZ_L1_OFFSET), TILE_SIZE_BYTE);            //Trigger DMA transaction: move Z tile from L1 to HBM
-                    flex_dma_async_wait_all();                                                              //Wait all DMA transaction done     
+                    //Trigger DMA transaction: move Z tile from L1 to HBM
+                    flex_dma_async_1d(hbm_addr(Z_hbm_addr),local(YZ_L1_OFFSET), TILE_SIZE_BYTE);
+
+                    //Wait all DMA transaction done
+                    flex_dma_async_wait_all();
                 }
-                flex_intra_cluster_sync();                                                                  //Cluster barrier
+                flex_intra_cluster_sync();//Cluster barrier
             }
         }
     }
 
-    flex_global_barrier_xy();                                                                               //Global barrier
+    flex_global_barrier_xy();//Global barrier
 
 }
 
