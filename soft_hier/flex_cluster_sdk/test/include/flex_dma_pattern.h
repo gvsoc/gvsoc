c28cc865f916d4ee97dbaf225d4feb66150f3978
Zhang Chi <chizhang@iis.ee.ethz.ch>
2024-11-08 11:59:15 +0100

    [SoftHier] clean up dma runtime

--- soft_hier/flex_cluster_sdk/test/include/flex_dma_pattern.h
+++ soft_hier/flex_cluster_sdk/test/include/flex_dma_pattern.h
@@ -5,6 +5,106 @@
 #include "flex_runtime.h"
 #include "flex_cluster_arch.h"
 
+/*************************************
+*  Basic Asynchronize DMA Interface  *
+*************************************/
+
+//Basic DMA 1d transfter load from HBM
+void flex_dma_async_1d(uint32_t dst_addr, uint32_t src_addr, size_t transfer_size){
+    flex_push_stack();
+    snrt_dma_start_1d(dst_addr, src_addr, transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//wait for idma
+void flex_dma_async_wait_all(){
+    flex_push_stack();
+    snrt_dma_wait_all(); // Wait for iDMA Finishing
+    flex_pull_stack();
+}
+
+/****************************************
+*  Versitle Asynchronize DMA Functions  *
+****************************************/
+
+
+void flex_dma_async_Load_HBM_1d(uint32_t local_offset, uint32_t hbm_offset, size_t transfer_size){
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),hbm_addr(hbm_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//Basic DMA 1d transfter store to HBM
+void flex_dma_async_Store_HBM_1d(uint32_t local_offset, uint32_t hbm_offset, size_t transfer_size){
+    flex_push_stack();
+    snrt_dma_start_1d(hbm_addr(hbm_offset), local(local_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+/*******************************************
+*  Traffic Pattern: Asynchronize Interface *
+*******************************************/
+
+//Pattern: Round Shift Right
+void flex_dma_async_pattern_round_shift_right(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),remote_pos(left_pos(pos),remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//Pattern: Round Shift Left
+void flex_dma_async_pattern_round_shift_left(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),remote_pos(right_pos(pos),remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+void flex_dma_async_pattern_round_shift_up(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),remote_pos(bottom_pos(pos),remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+
+//Pattern All-to-One
+void flex_dma_async_pattern_all_to_one(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),remote_xy(0,0,remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//Pattern Dialog-to-Dialog
+void flex_dma_async_pattern_dialog_to_dialog(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),remote_xy(pos.y,pos.x,remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//Pattern Access West HBM
+void flex_dma_async_pattern_access_west_hbm(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),hbm_west(pos.y,remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+//Pattern Access South HBM
+void flex_dma_async_pattern_access_south_hbm(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
+    FlexPosition pos = get_pos(flex_get_cluster_id());
+    flex_push_stack();
+    snrt_dma_start_1d(local(local_offset),hbm_south(pos.x,remote_offset), transfer_size); //Start iDMA
+    flex_pull_stack();
+}
+
+/******************************************
+*  Traffic Pattern: Synchronize Interface *
+******************************************/
+
 //Pattern: Round Shift Right
 void flex_dma_pattern_round_shift_right(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
     FlexPosition pos = get_pos(flex_get_cluster_id());
@@ -115,93 +215,6 @@ void flex_dma_pattern_systolic_shift_west_south(uint32_t local_offset, uint32_t 
         snrt_dma_start_1d(local(local_offset),remote_pos(bottom_pos(pos),remote_offset), transfer_size);
     }
 
-    snrt_dma_wait_all(); // Wait for iDMA Finishing
-    flex_pull_stack();
-}
-
-/***************************
-*  Asynchronize Iterface   *
-***************************/
-
-//Basic DMA 1d transfter load from HBM
-void flex_dma_async_1d(uint32_t src_addr, uint32_t dst_addr, size_t transfer_size){
-    flex_push_stack();
-    snrt_dma_start_1d(src_addr,dst_addr, transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-void flex_dma_async_Load_HBM_1d(uint32_t local_offset, uint32_t hbm_offset, size_t transfer_size){
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),hbm_addr(hbm_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Basic DMA 1d transfter store to HBM
-void flex_dma_async_Store_HBM_1d(uint32_t local_offset, uint32_t hbm_offset, size_t transfer_size){
-    flex_push_stack();
-    snrt_dma_start_1d(hbm_addr(hbm_offset), local(local_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Pattern: Round Shift Right
-void flex_dma_async_pattern_round_shift_right(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),remote_pos(left_pos(pos),remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Pattern: Round Shift Left
-void flex_dma_async_pattern_round_shift_left(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),remote_pos(right_pos(pos),remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-void flex_dma_async_pattern_round_shift_up(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),remote_pos(bottom_pos(pos),remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-
-//Pattern All-to-One
-void flex_dma_async_pattern_all_to_one(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),remote_xy(0,0,remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Pattern Dialog-to-Dialog
-void flex_dma_async_pattern_dialog_to_dialog(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),remote_xy(pos.y,pos.x,remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Pattern Access West HBM
-void flex_dma_async_pattern_access_west_hbm(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),hbm_west(pos.y,remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//Pattern Access South HBM
-void flex_dma_async_pattern_access_south_hbm(uint32_t local_offset, uint32_t remote_offset, size_t transfer_size){
-    FlexPosition pos = get_pos(flex_get_cluster_id());
-    flex_push_stack();
-    snrt_dma_start_1d(local(local_offset),hbm_south(pos.x,remote_offset), transfer_size); //Start iDMA
-    flex_pull_stack();
-}
-
-//wait for idma
-void flex_dma_async_wait_all(){
-    flex_push_stack();
     snrt_dma_wait_all(); // Wait for iDMA Finishing
     flex_pull_stack();
 }
