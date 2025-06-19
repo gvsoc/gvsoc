4fa98d52c8c8c8a37c8bc887515dae1ad460cb2c
bowwang <bowwang@iis.ee.ethz.ch>
2025-06-19 20:19:20 +0200

    [sw] add AspB_matmul application

--- examples/SoftHier/assembled/Spatz_GEMM/software/include/spatz_matmul.h
+++ examples/SoftHier/assembled/Spatz_GEMM/software/include/spatz_matmul.h
@@ -16,8 +16,11 @@
 
 // Author: Bowen Wang, ETH Zurich
 
-// Dense MatMul: A x B = C, (MxN) x (NxP) --> (MxP)
-// Data Width:   8-bit
+// Spatz MatMul library: A x B = C, (MxN) x (NxP) --> (MxP)
+//
+//
+// spatz_matmul_fp8():  dense matmul [Gustav], input _fp8, output _fp8
+// spatz_matmul_fp16(): dense matmul [Gustav], input _fp8, output _fp16
 
 #ifndef _SPATZ_GEMM_H_
 #define _SPATZ_GEMM_H_
@@ -28,7 +31,7 @@
 
 #pragma GCC optimize("no-tree-loop-distribute-patterns")
 
-// outer product 
+// dense matmul [Gustav], input _fp8, output _fp8
 void spatz_matmul_fp8(uint8_t* matrix_a, uint8_t* matrix_b, uint8_t* matrix_c, 
                   const uint32_t M, const uint32_t N, const uint32_t P){
 
@@ -64,6 +67,7 @@ void spatz_matmul_fp8(uint8_t* matrix_a, uint8_t* matrix_b, uint8_t* matrix_c,
     } while (avl>0);
 }
 
+// dense matmul [Gustav], input _fp8, output _fp16
 void spatz_matmul_fp16(uint8_t* matrix_a, uint8_t* matrix_b, uint16_t* matrix_c, 
                   const uint32_t M, const uint32_t N, const uint32_t P){
 
@@ -97,7 +101,68 @@ void spatz_matmul_fp16(uint8_t* matrix_a, uint8_t* matrix_b, uint16_t* matrix_c,
         avl -= vl;
         p += vl;
     } while (avl>0);
+}
 
+// sparse matmul [Gustav], AspB, input _fp8, index _uint8, output _fp16
+// effective output elements are gather/scatter with VLSU
+void spatz_AspB_matmul_fp16(uint8_t* matrix_a, uint8_t* matrix_b, uint16_t* matrix_c, uint8_t* index_b,
+                  const uint32_t M, const uint32_t N, const uint32_t P, 
+                  const uint8_t spN, const uint8_t spM){
+
+    // init counters
+    uint32_t p = 0;
+
+    uint32_t avl = P * spN / spM;
+    uint32_t vl;
+    uint32_t pre_vl = 0;
+    do{
+        asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(vl) : "r"(avl));
+        if (pre_vl != vl){    // vl changed, index re-calculate, otherwise skip
+            for (uint32_t i=0; i<N; i++){                        // row
+                for (uint32_t j=0; j<((P*spN/spM)/vl); j++){     // vl block
+                    for (uint32_t k=0; k<vl/spN; k++){           // nm block
+                        for (uint32_t q=0; q<spN; q++) index_b[q + k*spN + j*vl + i*(P*spN/spM)] += k*spM;
+                    }
+                }
+            }
+            pre_vl = vl;
+        }
+        
+        for (uint32_t m = 0; m < M; m++){
+            uint16_t *p_c = matrix_c + m*P + p;
+            for (uint32_t n = 0; n < N; n++){
+                // load a
+                uint8_t *p_a = &matrix_a[m*N + n];
+                asm volatile("flw fa0, (%0)" :: "r"(p_a));
+
+                // load b vec
+                uint8_t *p_b = &matrix_b[p + n*P*spN/spM];
+                asm volatile("vle8.v v0, (%0)" ::"r"(p_b));
+
+                // load index 
+                uint8_t *p_index = &index_b[p + n*P*spN/spM];
+                asm volatile("vle8.v v8, (%0)" ::"r"(p_index));
+
+                if (n==0){ // first iter
+                    asm volatile("vmv.v.i v16, 0");
+                    asm volatile("vfwmul.vf v16, v0, fa0");
+                    asm volatile("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                    asm volatile("vsuxei8.v v16, (%0), v8" ::"r"(p_c));
+                    asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                } else {
+                    asm volatile("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                    asm volatile("vluxei8.v v16, (%0), v8" ::"r"(p_c));
+                    asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                    asm volatile("vfwmacc.vf v16, fa0, v0");
+                    asm volatile("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                    asm volatile("vsuxei8.v v16, (%0), v8" ::"r"(p_c));
+                    asm volatile("vsetvli %0, %1, e8, m8, ta, ma" : "=r"(vl) : "r"(avl));
+                }
+            }
+        }
+        avl -= vl;
+        p += vl;
+    } while (avl>0);
 }
 
 #endif
