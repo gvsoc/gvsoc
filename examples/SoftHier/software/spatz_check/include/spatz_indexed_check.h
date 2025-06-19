d21a99e422622bf3e0b70c23b048ebe4732133aa
bowwang <bowwang@iis.ee.ethz.ch>
2025-06-19 20:18:30 +0200

    [test] add vsuxei8.v instruction check

--- examples/SoftHier/software/spatz_check/include/spatz_indexed_check.h
+++ examples/SoftHier/software/spatz_check/include/spatz_indexed_check.h
@@ -5,7 +5,7 @@
 #include "flex_printf.h"
 #include "flex_libfp16.h"
 
-#define _AVL (256)
+#define _AVL (64)
 #pragma GCC optimize("no-tree-loop-distribute-patterns")
 
 void l1_layout(uint8_t *addr, uint8_t size){ // size in byte
@@ -388,6 +388,71 @@ uint8_t test_ele64_index8(void * ref_inp, void * ref_ind, void * ref_oup){
     return err;
 }
 
+// index store
+uint8_t test_st_ele16_index8(void * ref_inp, void * ref_ind, void * ref_oup){
+    uint32_t vl;
+    uint32_t avl;
+    printf("[Index Store Tests: 16-bit ele, 8-bit index]\n");
+    uint16_t  * inp = (uint16_t *)ref_inp;
+    uint8_t   * ind = (uint8_t *)ref_ind;
+    uint16_t  * oup = (uint16_t *)ref_oup;
+    uint16_t  * exp = (uint16_t *)local(0x8000);
+
+    // init vector
+    for (int i = 0; i < _AVL; ++i) inp[i] = i+1;
+    printf("[Init inp, done]\n");
+    // init output
+    for (int i = 0; i < 8 * _AVL; ++i) {
+        oup[i] = 0;
+        exp[i] = 0;
+    }
+    printf("[Init exp, oup, done]\n");
+    // init index
+    for (int i = 0; i < _AVL; ++i) ind[i] = 3*i+1;
+    printf("[Init ind, done]\n");
+    // calculate expected output
+    for (int i = 0; i < _AVL; ++i){
+        exp[ind[i]] = inp[i];
+    }
+
+    // index load
+    avl = _AVL;
+    do{
+        asm volatile("vsetvli %0, %1, e16, m2, ta, ma" : "=r"(vl) : "r"(avl));
+        asm volatile("vle8.v  v0, (%0)" ::"r"(ind));
+        asm volatile("vle16.v v2, (%0)" ::"r"(inp));
+        asm volatile("vsuxei8.v v2, (%0), v0" ::"r"(oup));
+        avl -= vl;
+    } while (avl>0);
+
+    printf("[Check indexed load]\n");
+    uint8_t err = 0;
+    for (int i = 0; i < 8*_AVL; ++i){
+        if (exp[i] != oup[i]){
+            err+=1;
+            printf("exp: %d, oup: %d\n", exp[i], oup[i]);
+        } 
+    }
+    printf(">>> %d error(s) out of %d checks\n\n", err, _AVL);
+
+    // for(uint8_t i=0; i<2*_AVL; i++){
+    //     if (i%8==0) printf("\n");
+    //     printf("%4d", oup[i]);
+    // }
+
+    // clean up
+    for (int i = 0; i < _AVL; ++i){
+        for (int j=0; j<8; j++){
+            inp[i*8+j] = 0;
+        }
+        ind[i] = 0;
+        exp[i] = 0;
+        oup[i] = 0;
+    }
+    return err;
+}
+
+
 void test_spatz_indexed(){
     flex_global_barrier_xy();//Global barrier
     if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0)
@@ -399,26 +464,27 @@ void test_spatz_indexed(){
         void * ref_inp = (void *)local(0x2000);
         void * ref_ind = (void *)local(0x4000);
         void * ref_oup = (void *)local(0x6000);
-        tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index16(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index32(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index64(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele16_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele32_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele64_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index16(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index32(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index64(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele16_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele32_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele64_index8(ref_inp, ref_ind, ref_oup);
+        tot_err += test_st_ele16_index8(ref_inp, ref_ind, ref_oup);
 
-        ref_inp = (void *)local(0x2003);
-        tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index16(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index32(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele8_index64(ref_inp, ref_ind, ref_oup);
+        // ref_inp = (void *)local(0x2003);
+        // tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index16(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index32(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele8_index64(ref_inp, ref_ind, ref_oup);
 
-        ref_inp = (void *)local(0x2000);
-        ref_ind = (void *)local(0x4005);
-        tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele16_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele32_index8(ref_inp, ref_ind, ref_oup);
-        tot_err += test_ele64_index8(ref_inp, ref_ind, ref_oup);
+        // ref_inp = (void *)local(0x2000);
+        // ref_ind = (void *)local(0x4005);
+        // tot_err += test_ele8_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele16_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele32_index8(ref_inp, ref_ind, ref_oup);
+        // tot_err += test_ele64_index8(ref_inp, ref_ind, ref_oup);
 
         printf(">>> tot_error: %d\n", tot_err);
     }
