a356d3ecef77abd9fa9695fb1ae60b23ec72a42b
Zhang Chi <chizhang@iis.ee.ethz.ch>
2025-06-26 09:21:52 +0200

    [SoftHier DSv3] add MoE GEMM part

--- examples/SoftHier/assembled/DeepSeekV3/software/main.c
+++ examples/SoftHier/assembled/DeepSeekV3/software/main.c
@@ -6,7 +6,7 @@
 #include "deepseek_summa_2d.h"
 #include <math.h>
 
-#define SEQ_LEN 8192
+#define SEQ_LEN 4096
 #define HBM_INTERVAL_OFFSET (DEEPSEEK_EMBEDDED_LENGTH * SEQ_LEN)
 
 #define SUMMA_M_TILE 128
@@ -237,93 +237,93 @@ int main()
     /*                          Step 3. Meta Q^C                                         */
     /*************************************************************************************/
 
-    if (SEQ_LEN == 512)
-    {
-        // 512-65536-1536
-        DeepSeek_SUMMA_2D_Weight_Stationary(
-            hbm_west(0,0)/*X_address*/,
-            hbm_south(0,0)/*W_address*/,
-            hbm_west(0,0)/*Z_address*/,
-            512/*M_size*/,
-            8192/*N_size*/,
-            1536/*K_size*/, /*shared dimension*/
-            128/*M_tile*/,
-            128/*N_tile*/,
-            128/*K_tile*/,
-            32/*group_x*/,
-            4/*group_y*/,
-            8/*num_group*/,
-            0/*group_reduction*/,
-            0/*X_address_group_gap*/,
-            0/*W_address_group_gap*/,
-            0/*Z_address_group_gap*/);
-    }
+    // if (SEQ_LEN == 512)
+    // {
+    //     // 512-65536-1536
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         512/*M_size*/,
+    //         8192/*N_size*/,
+    //         1536/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         128/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         4/*group_y*/,
+    //         8/*num_group*/,
+    //         0/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
         
-    if (SEQ_LEN == 1024)
-    {
-        // 1024-65536-1536
-        DeepSeek_SUMMA_2D_Weight_Stationary(
-            hbm_west(0,0)/*X_address*/,
-            hbm_south(0,0)/*W_address*/,
-            hbm_west(0,0)/*Z_address*/,
-            1024/*M_size*/,
-            16384/*N_size*/,
-            1536/*K_size*/, /*shared dimension*/
-            128/*M_tile*/,
-            128/*N_tile*/,
-            128/*K_tile*/,
-            32/*group_x*/,
-            8/*group_y*/,
-            4/*num_group*/,
-            0/*group_reduction*/,
-            0/*X_address_group_gap*/,
-            0/*W_address_group_gap*/,
-            0/*Z_address_group_gap*/);
-    }
+    // if (SEQ_LEN == 1024)
+    // {
+    //     // 1024-65536-1536
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         1024/*M_size*/,
+    //         16384/*N_size*/,
+    //         1536/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         128/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         8/*group_y*/,
+    //         4/*num_group*/,
+    //         0/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
 
-    if (SEQ_LEN == 2048)
-    {
-        // 2048-65536-1536
-        DeepSeek_SUMMA_2D_Weight_Stationary(
-            hbm_west(0,0)/*X_address*/,
-            hbm_south(0,0)/*W_address*/,
-            hbm_west(0,0)/*Z_address*/,
-            2048/*M_size*/,
-            32768/*N_size*/,
-            1536/*K_size*/, /*shared dimension*/
-            128/*M_tile*/,
-            128/*N_tile*/,
-            128/*K_tile*/,
-            32/*group_x*/,
-            16/*group_y*/,
-            2/*num_group*/,
-            0/*group_reduction*/,
-            0/*X_address_group_gap*/,
-            0/*W_address_group_gap*/,
-            0/*Z_address_group_gap*/);
-    }
+    // if (SEQ_LEN == 2048)
+    // {
+    //     // 2048-65536-1536
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         2048/*M_size*/,
+    //         32768/*N_size*/,
+    //         1536/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         128/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         16/*group_y*/,
+    //         2/*num_group*/,
+    //         0/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
 
-    if (SEQ_LEN == 4096)
-    {
-        // 4096-65536-1536
-        DeepSeek_SUMMA_2D_Weight_Stationary(
-            hbm_west(0,0)/*X_address*/,
-            hbm_south(0,0)/*W_address*/,
-            hbm_west(0,0)/*Z_address*/,
-            4096/*M_size*/,
-            65536/*N_size*/,
-            1536/*K_size*/, /*shared dimension*/
-            128/*M_tile*/,
-            128/*N_tile*/,
-            128/*K_tile*/,
-            32/*group_x*/,
-            32/*group_y*/,
-            1/*num_group*/,
-            0/*group_reduction*/,
-            0/*X_address_group_gap*/,
-            0/*W_address_group_gap*/,
-            0/*Z_address_group_gap*/);
-    }
+    // if (SEQ_LEN == 4096)
+    // {
+    //     // 4096-65536-1536
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         4096/*M_size*/,
+    //         65536/*N_size*/,
+    //         1536/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         128/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         32/*group_y*/,
+    //         1/*num_group*/,
+    //         0/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
 
     /*************************************************************************************/
     /*                          Step 4. Meta Q^R                                         */
@@ -407,8 +407,280 @@ int main()
     //     0/*Z_address_group_gap*/);
 
     /*************************************************************************************/
-    /*                          Step 4. Output_Proj                                      */
+    /*                          Step 5. Output_Proj                                      */
     /*************************************************************************************/
+
+    // if (SEQ_LEN == 512)
+    // {
+    //     // 512-7168-16384
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         512/*M_size*/,
+    //         7168/*N_size*/,
+    //         2048/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         224/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         4/*group_y*/,
+    //         8/*num_group*/,
+    //         1/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
+
+    // if (SEQ_LEN == 1024)
+    // {
+    //     // 1024-7168-16384
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         1024/*M_size*/,
+    //         7168/*N_size*/,
+    //         4096/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         224/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         8/*group_y*/,
+    //         4/*num_group*/,
+    //         1/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
+
+    // if (SEQ_LEN == 2048)
+    // {
+    //     // 2048-7168-16384
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         2048/*M_size*/,
+    //         7168/*N_size*/,
+    //         8192/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         224/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         16/*group_y*/,
+    //         2/*num_group*/,
+    //         1/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
+
+    // if (SEQ_LEN == 4096)
+    // {
+    //     // 4096-7168-16384
+    //     DeepSeek_SUMMA_2D_Weight_Stationary(
+    //         hbm_west(0,0)/*X_address*/,
+    //         hbm_south(0,0)/*W_address*/,
+    //         hbm_west(0,0)/*Z_address*/,
+    //         4096/*M_size*/,
+    //         7168/*N_size*/,
+    //         16384/*K_size*/, /*shared dimension*/
+    //         128/*M_tile*/,
+    //         128/*N_tile*/,
+    //         128/*K_tile*/,
+    //         32/*group_x*/,
+    //         32/*group_y*/,
+    //         1/*num_group*/,
+    //         0/*group_reduction*/,
+    //         0/*X_address_group_gap*/,
+    //         0/*W_address_group_gap*/,
+    //         0/*Z_address_group_gap*/);
+    // }
+
+    /*************************************************************************************/
+    /*                          Step 6. MoE_Front                                        */
+    /*************************************************************************************/
+
+    if (SEQ_LEN == 512)
+    {
+        // 2048-4096-7168
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            512/*M_size*/,
+            4096/*N_size*/,
+            896/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            128/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            4/*group_y*/,
+            8/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 1024)
+    {
+        // 2048-4096-7168
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            1024/*M_size*/,
+            4096/*N_size*/,
+            1792/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            128/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            8/*group_y*/,
+            4/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 2048)
+    {
+        // 2048-4096-7168
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            2048/*M_size*/,
+            4096/*N_size*/,
+            3584/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            128/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            16/*group_y*/,
+            2/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 4096)
+    {
+        // 4096-4096-7168
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            4096/*M_size*/,
+            4096/*N_size*/,
+            7168/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            128/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            32/*group_y*/,
+            1/*num_group*/,
+            0/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    /*************************************************************************************/
+    /*                          Step 7. MoE_Back                                         */
+    /*************************************************************************************/
+
+    if (SEQ_LEN == 512)
+    {
+        // 512-7168-2048
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            512/*M_size*/,
+            7168/*N_size*/,
+            256/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            224/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            4/*group_y*/,
+            8/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 1024)
+    {
+        // 1024-7168-2048
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            1024/*M_size*/,
+            7168/*N_size*/,
+            512/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            224/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            8/*group_y*/,
+            4/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 2048)
+    {
+        // 2048-7168-2048
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            2048/*M_size*/,
+            7168/*N_size*/,
+            1024/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            224/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            16/*group_y*/,
+            2/*num_group*/,
+            1/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
+
+    if (SEQ_LEN == 4096)
+    {
+        // 4096-7168-2048
+        DeepSeek_SUMMA_2D_Weight_Stationary(
+            hbm_west(0,0)/*X_address*/,
+            hbm_south(0,0)/*W_address*/,
+            hbm_west(0,0)/*Z_address*/,
+            4096/*M_size*/,
+            7168/*N_size*/,
+            2048/*K_size*/, /*shared dimension*/
+            128/*M_tile*/,
+            128/*N_tile*/,
+            128/*K_tile*/,
+            32/*group_x*/,
+            32/*group_y*/,
+            1/*num_group*/,
+            0/*group_reduction*/,
+            0/*X_address_group_gap*/,
+            0/*W_address_group_gap*/,
+            0/*Z_address_group_gap*/);
+    }
 
 
     /**************************************/
