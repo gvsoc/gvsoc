49207bafaf58b93b83302f5b7c6e69e8e9798063
Germain Haugou <germain.haugou@gmail.com>
2025-10-23 20:27:54 +0200

    ci: sdk and tests from core repo can now be built out-of-tree

--- sourceme.sh
+++ sourceme.sh
@@ -1,12 +1,19 @@
 if [  -n "${ZSH_VERSION:-}" ]; then
-	DIR="$(readlink -f -- "${(%):-%x}")"
-	SDK_HOME=$(dirname $DIR)
+   	DIR="$(readlink -f -- "${(%):-%x}")"
+   	SDK_HOME=$(dirname $DIR)
 else
-	SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+   	SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 fi
 
-export PATH=$SDK_HOME/install/bin:$PATH
-export PYTHONPATH=$SDK_HOME/install/python:$PYTHONPATH
+if [ -n "${GVSOC_WORKDIR}" ]; then
+    SDK_INSTALL=${GVSOC_WORKDIR}
+else
+    SDK_INSTALL=${SDK_HOME}
+fi
+
+export PATH=$SDK_INSTALL/install/bin:$PATH
+export PYTHONPATH=$SDK_INSTALL/install/python:$PYTHONPATH
+export PULPOS_HOME=$SDK_HOME/pulpos
 
 ##############################################################################
 ## 				Envirment Parameters for DRAMSys Integration 				##
