85286a44fffcf322a45b0f71901d4a4a211245c7
Zhang Chi <chizhang@iis.ee.ethz.ch>
2026-05-04 18:56:51 +0200

    [SoftHier] add configurable linker script for HBM addressing

--- soft_hier/flex_cluster_utilities/config.py
+++ soft_hier/flex_cluster_utilities/config.py
@@ -89,3 +89,84 @@ with open(S_header_file, 'w') as file:
     file.write('\n#endif // FLEXCLUSTERARCH_H\n')
 
 print(f'Header file "{S_header_file}" generated successfully.')
+
+#Generate the memory.ld file
+def generate_memory_ld(
+        l1_base,
+        l1_size,
+        l3_base,
+        l3_size,
+        hbm_west_base,
+        hbm_west_size,
+        hbm_north_base,
+        hbm_north_size,
+        hbm_east_base,
+        hbm_east_size,
+        hbm_south_base,
+        hbm_south_size):
+    ld_content = f"""
+MEMORY
+{{
+    L1  (rwxa) : ORIGIN = 0x{l1_base:016X}, LENGTH = 0x{l1_size:016X}
+    L3  (rwxa) : ORIGIN = 0x{l3_base:016X}, LENGTH = 0x{l3_size:016X}
+    HBM_WEST  (rwxa) : ORIGIN = 0x{hbm_west_base:016X}, LENGTH = 0x{hbm_west_size:016X}
+    HBM_NORTH (rwxa) : ORIGIN = 0x{hbm_north_base:016X}, LENGTH = 0x{hbm_north_size:016X}
+    HBM_EAST  (rwxa) : ORIGIN = 0x{hbm_east_base:016X}, LENGTH = 0x{hbm_east_size:016X}
+    HBM_SOUTH (rwxa) : ORIGIN = 0x{hbm_south_base:016X}, LENGTH = 0x{hbm_south_size:016X}
+}}
+"""
+    return ld_content
+with open('soft_hier/flex_cluster_sdk/runtime/memory.ld', 'w') as file:
+    num_cluster_x = 0
+    num_cluster_y = 0
+    cluster_tcdm_base = 0
+    cluster_tcdm_size = 0
+    instruction_mem_base = 0
+    instruction_mem_size = 0
+    hbm_start_base = 0
+    hbm_node_addr_space = 0
+    for attr_name, attr_value in attributes.items():
+        if attr_name == 'num_cluster_x':
+            num_cluster_x = int(attr_value)
+        if attr_name == 'num_cluster_y':
+            num_cluster_y = int(attr_value)
+        if attr_name == 'cluster_tcdm_base':
+            cluster_tcdm_base = int(attr_value, 16)
+        if attr_name == 'cluster_tcdm_size':
+            cluster_tcdm_size = int(attr_value, 16)
+        if attr_name == 'instruction_mem_base':
+            instruction_mem_base = int(attr_value, 16)
+        if attr_name == 'instruction_mem_size':
+            instruction_mem_size = int(attr_value, 16)
+        if attr_name == 'hbm_start_base':
+            hbm_start_base = int(attr_value, 16)
+        if attr_name == 'hbm_node_addr_space':
+            hbm_node_addr_space = int(attr_value, 16)
+    l1_base = cluster_tcdm_base
+    l1_size = cluster_tcdm_size
+    l3_base = instruction_mem_base
+    l3_size = instruction_mem_size
+    hbm_west_base = hbm_start_base
+    hbm_west_size = num_cluster_y * hbm_node_addr_space
+    hbm_north_base = hbm_west_base + hbm_west_size
+    hbm_north_size = num_cluster_x * hbm_node_addr_space
+    hbm_east_base = hbm_north_base + hbm_north_size
+    hbm_east_size = num_cluster_y * hbm_node_addr_space
+    hbm_south_base = hbm_east_base + hbm_east_size
+    hbm_south_size = num_cluster_x * hbm_node_addr_space
+    ld_content = generate_memory_ld(
+        l1_base,
+        l1_size,
+        l3_base,
+        l3_size,
+        hbm_west_base,
+        hbm_west_size,
+        hbm_north_base,
+        hbm_north_size,
+        hbm_east_base,
+        hbm_east_size,
+        hbm_south_base,
+        hbm_south_size)
+    file.write(ld_content)
+
+print('Linker script "memory.ld" generated successfully.')
